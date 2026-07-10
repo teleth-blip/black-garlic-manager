3f56e2307d12c24d2411c5b9752f602b73e35ae4
teleth-blip <teleth@gmail.com>
2026-07-11 08:58:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://yedrlbrzrkbtgswzplia.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllZHJsYnJ6cmtidGdzd3pwbGlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NjQ4NzYsImV4cCI6MjA5NzA0MDg3Nn0.mjT1hGDQZ-Sd1duCsf6GZE3Eontx757Nx7-LQqQ6YsE"
 };
